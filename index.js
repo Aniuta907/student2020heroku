da9a23302fa65151317d7887aa66aad129c1abf4
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2020-10-09 20:59:39 +0300

    inut commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('http')
   res.writeHead(200, CORS);
   if (req.url === '/v8') return res.end(process.versions.v8);
   if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
+  if (req.url === '/login') return res.end('annakuznetsova');
   
   res.end('student2020heroku');
 })
