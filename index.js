692555c0c6a2d2c1ce734c957e1b3e4dc3578868
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-10-09 18:25:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 require('http')
-.Server()
+.Server((req, res) => {
+  if (req.url === '/v8') return res.end(process.versions.v8);
+  
+  res.end('student2020heroku');
+})
 .listen(process.env.PORT);
