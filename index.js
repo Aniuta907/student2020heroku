6ef743aae07db55aef019b077a31e138d759fc00
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-10-09 18:34:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('http')
 .Server((req, res) => {
   if (req.url === '/v8') return res.end(process.versions.v8);
+  if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
   
   res.end('student2020heroku');
 })
