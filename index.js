c01a0bc8431a045e0e117d25ab3cc2ba41da8cb0
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-10-09 19:16:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 require('http')
 .Server((req, res) => {
+  
+  const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+  res.writeHead(200, CORS);
   if (req.url === '/v8') return res.end(process.versions.v8);
   if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
   
